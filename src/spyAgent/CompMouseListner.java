a3c718ec69049bae3a7579fff17ffe7b3ffbc724
muresanrazvan <rmuresan@nokia.com>
2017-04-05 14:11:10 +0300

    fixed bug for awt components (properties not displayed)

--- src/spyAgent/CompMouseListner.java
+++ src/spyAgent/CompMouseListner.java
@@ -71,7 +71,7 @@ public class CompMouseListner implements MouseListener {
                 int index = compProps.indexOf("[");
                 String name = compProps.substring(index + 1, compProps.indexOf(','));
                 KeyboardListener.highlightedComponentName = name;
-                JComponent comp = (JComponent) arg0.getComponent();
+                Component comp = arg0.getComponent();
                 Communicator.writeToServer("Window Title - " + winTitle + ",Index - " + myIndex + ",Instance Of - " + classType + ",Comp. Hierarchy - " + compHierarchy + "," + compProps);
                 highlightedComponentColor = comp.getBackground();
                 comp.setBackground(new Color(90, 100, 210));
@@ -85,7 +85,7 @@ public class CompMouseListner implements MouseListener {
     public void mouseExited(MouseEvent arg0) {
         if (flag) {
             try {
-                JComponent comp = (JComponent) arg0.getComponent();
+                Component comp = arg0.getComponent();
                 comp.setBackground(highlightedComponentColor);
                 flag = false;
             } catch (Exception e) {
--- src/spyAgent/HierarchyMap.java
+++ src/spyAgent/HierarchyMap.java
@@ -7,7 +7,7 @@
 package spyAgent;
 
 
-import java.awt.Component;
+import java.awt.*;
 import java.util.HashMap;
 
 import javax.swing.*;
@@ -37,139 +37,98 @@ public class HierarchyMap {
     public String getInstance(Component comp) {
         if (comp instanceof JButton) {
             instance = "JButton";
-            props = ((JButton) comp).toString();
         } else if (comp instanceof JCheckBox) {
             instance = "JCheckBox";
-            props = ((JCheckBox) comp).toString();
         } else if (comp instanceof JCheckBoxMenuItem) {
             instance = "JCheckBoxMenuItem";
-            props = ((JCheckBoxMenuItem) comp).toString();
         } else if (comp instanceof JColorChooser) {
             instance = "JColorChooser";
-            props = ((JColorChooser) comp).toString();
         } else if (comp instanceof JComboBox) {
             instance = "JComboBox";
-            props = ((JComboBox) comp).toString();
         } else if (comp instanceof JDesktopPane) {
             instance = "JDesktopPane";
-            props = ((JDesktopPane) comp).toString();
         } else if (comp instanceof JDialog) {
             instance = "JDialog";
-            props = ((JDialog) comp).toString();
         } else if (comp instanceof JEditorPane) {
             instance = "JEditorPane";
-            props = ((JEditorPane) comp).toString();
         } else if (comp instanceof JFileChooser) {
             instance = "JFileChooser";
-            props = ((JFileChooser) comp).toString();
         } else if (comp instanceof JFrame) {
             instance = "JFrame";
-            props = ((JFrame) comp).toString();
         } else if (comp instanceof JInternalFrame) {
             instance = "JInternalFrame";
-            props = ((JInternalFrame) comp).toString();
         } else if (comp instanceof JInternalFrame.JDesktopIcon) {
             instance = "JInternalFrame.JDesktopIcon";
-            props = ((JInternalFrame.JDesktopIcon) comp).toString();
         } else if (comp instanceof JLabel) {
             instance = "JLabel";
-            props = ((JLabel) comp).toString();
         } else if (comp instanceof JLayeredPane) {
             instance = "JLayeredPane";
-            props = ((JLayeredPane) comp).toString();
         } else if (comp instanceof JList) {
             instance = "JList";
-            props = ((JList) comp).toString();
         } else if (comp instanceof JMenu) {
             instance = "JMenu";
-            props = ((JMenu) comp).toString();
         } else if (comp instanceof JMenuBar) {
             instance = "JMenuBar";
-            props = ((JMenuBar) comp).toString();
         } else if (comp instanceof JMenuItem) {
             instance = "JMenuItem";
-            props = ((JMenuItem) comp).toString();
         } else if (comp instanceof JOptionPane) {
             instance = "JOptionPane";
-            props = ((JOptionPane) comp).toString();
         } else if (comp instanceof JPanel) {
             instance = "JPanel";
-            props = ((JPanel) comp).toString();
         } else if (comp instanceof JPopupMenu) {
             instance = "JPopupMenu";
-            props = ((JPopupMenu) comp).toString();
         } else if (comp instanceof JProgressBar) {
             instance = "JProgressBar";
-            props = ((JProgressBar) comp).toString();
         } else if (comp instanceof JRadioButton) {
             instance = "JRadioButton";
-            props = ((JRadioButton) comp).toString();
         } else if (comp instanceof JRadioButtonMenuItem) {
             instance = "JRadioButtonMenuItem";
-            props = ((JRadioButtonMenuItem) comp).toString();
         } else if (comp instanceof JRootPane) {
             instance = "JRootPane";
-            props = ((JRootPane) comp).toString();
         } else if (comp instanceof JScrollBar) {
             instance = "JScrollBar";
-            props = ((JScrollBar) comp).toString();
         } else if (comp instanceof JScrollPane) {
             instance = "JScrollPane";
-            props = ((JScrollPane) comp).toString();
         } else if (comp instanceof JSeparator) {
             instance = "JSeparator";
-            props = ((JSeparator) comp).toString();
         } else if (comp instanceof JSlider) {
             instance = "JSlider";
-            props = ((JSlider) comp).toString();
         } else if (comp instanceof JSpinner) {
             instance = "JSpinner";
-            props = ((JSpinner) comp).toString();
         } else if (comp instanceof JSplitPane) {
             instance = "JSplitPane";
-            props = ((JSplitPane) comp).toString();
         } else if (comp instanceof JTabbedPane) {
             instance = "JTabbedPane";
-            props = ((JTabbedPane) comp).toString();
         } else if (comp instanceof JTable) {
             instance = "JTable";
-            props = ((JTable) comp).toString();
         } else if (comp instanceof JTextArea) {
             instance = "JTextArea";
-            props = ((JTextArea) comp).toString();
         } else if (comp instanceof JTextField) {
             instance = "JTextField";
-            props = ((JTextField) comp).toString();
         } else if (comp instanceof JTextPane) {
             instance = "JTextPane";
-            props = ((JTextPane) comp).toString();
         } else if (comp instanceof JToggleButton) {
             instance = "JToggleButton";
-            props = ((JToggleButton) comp).toString();
         } else if (comp instanceof JToolBar) {
             instance = "JToolBar";
-            props = ((JToolBar) comp).toString();
         } else if (comp instanceof JToolTip) {
             instance = "JToolTip";
-            props = ((JToolTip) comp).toString();
         } else if (comp instanceof JTree) {
             instance = "JTree";
-            props = ((JTree) comp).toString();
         } else if (comp instanceof JViewport) {
             instance = "JViewport";
-            props = ((JViewport) comp).toString();
         } else if (comp instanceof JWindow) {
             instance = "JWindow";
-            props = ((JWindow) comp).toString();
         } else if (comp instanceof JTableHeader) {
             instance = "JTableHeader";
-            props = ((JTableHeader) comp).toString();
         } else if (comp instanceof JComponent) {
             instance = "JComponent";
-            props = ((JComponent) comp).toString();
+        } else if (comp instanceof TextField) {
+            instance = "TextField";
         } else {
             instance = null;
         }
+        props = comp.toString();
         setIndex(instance);
         return instance;
     }
