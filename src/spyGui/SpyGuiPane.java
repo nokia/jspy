305558051bbad4f68cff6c7db29fb561ba53db79
Gugea <oana.gugea@nokia.com>
2017-11-27 13:58:55 +0200

    fixed issue #13

--- src/spyGui/SpyGuiPane.java
+++ src/spyGui/SpyGuiPane.java
@@ -83,6 +83,9 @@ public class SpyGuiPane extends JPanel {
         if (s.contains("=") || s.contains(":") || s.contains("-")){
             return true;
         }
+        if(s.matches("\\d+(X\\d+)?(x\\d+)?")) {
+            return true;
+        }
         return false;
     }
 
@@ -93,13 +96,21 @@ public class SpyGuiPane extends JPanel {
         } else if (s.contains("Client Disconnected.")) {
             tableModel.setRowCount(0);
             bottomTextPane.setText(s);
+        }else if(s.equals("new line")){
+            tableModel.addRow(new Object[]{null,null});
         } else if (isProperty(s)) {
             String name_value[] = s.split("[=:-]");
-            tableModel.addRow(name_value);
+            if(name_value.length < 2){
+                String emptyFirst[] = {"", name_value[0]};
+                tableModel.addRow(emptyFirst);
+            } else {
+                tableModel.addRow(name_value);
+            }
         } else {
             String splitText[] = s.split("\n");
             bottomTextPane.setText(splitText[0]);
         }
+
 
         scrollPane.validate();
         table.setModel(tableModel);
